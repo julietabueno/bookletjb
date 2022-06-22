ac6e86cd311a1194d817ce39faa74c60a41d935e
julietabueno <bueno.julieta.ok@gmail.com>
2022-06-22 10:54:33 -0300

    haciendo actividades de booklet 1

---
+++ script.js
@@ -0,0 +1,58 @@
+//Actividad 1
+
+/* let nombre = "Homero";
+let apellido = "Simpson";
+let edad = "35";
+
+console.log (nombre);
+console.log(apellido);
+console.log(edad);
+
+ */
+
+//Actividad 2
+
+/* const ciudad1 = "Springfield";
+const ciudad2 = "Buenos Aires";
+const ciudad3 = "La Pampa";
+const ciudad4 = "Escobar";
+const ciudad5 = "Vicente Lopez";
+console.log(ciudad1);
+console.log(ciudad2);
+console.log(ciudad3);
+console.log(ciudad4);
+console.log(ciudad5);
+
+ */
+//Actividad 3
+
+
+/* let nombreapellido    = "Julieta Bueno";
+let direccion  = "Barrio El Naudir";
+let pais       = "Argentina";
+let fnacimiento = "12-09-1984";
+const codigo   = "B47U89RE243";
+let carnet = "Codigo: " + codigo + " " +
+            "Nombre: " + nombreapellido + " " +
+            "Direccion: " + direccion + " " +
+            "Pais: " + pais + " " +
+            "Fecha de Nacimiento: " + fnacimiento;
+
+console.log(carnet);
+ */
+
+//Actividad 4
+
+let integrante1 = prompt('Ingrese integrante 1');
+let integrante2 = prompt('Ingrese integrante 2');
+let integrante3 = prompt('Ingrese integrante 3');
+let integrante4 = prompt('Ingrese integrante 4');
+let integrante5 = prompt('Ingrese integrante 5');
+let familia =   "1) "+integrante1+" "+
+                "2) "+integrante2+" "+
+                "3) "+integrante3+" "+
+                "4) "+integrante4+" "+
+                "5) "+integrante5;
+console.log(familia);
+
+
